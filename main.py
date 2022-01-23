bc2114ae09bf1c69c1989b6392d24fd94c92eca0
Ampatishan <ampatishan@Ampatishans-MacBook-Air.local>
2022-01-23 17:24:47 +0530

    completed upto calculating the posterior

--- main.py
+++ main.py
@@ -13,17 +13,36 @@ female_height = torch.randn(number_of_female) * 10 + 140
 
 
 def generate_pdf(input_data):
-    x = np.histogram(input_data, bins=int(max(input_data) - min(input_data)))
+    x = np.histogram(input_data, bins=range(100, 200))
     pdf = x[0] / len(input_data)
     bin_centers = (x[1][1:] + x[1][:-1]) / 2
     return pdf, bin_centers
 
-# height = torch.cat((male_height, female_height))
-# sns.distplot(p_male_height)
-# plt.show()
-# sns.distplot(male_height)
-# sns.distplot(female_height)
-# plt.show()
-#
-# sns.barplot(['m', 'f'], [number_of_male, number_of_female])
-# plt.show()
+
+# finding the likelihood P(X|H)
+p_x_given_male, bin_center_male = generate_pdf(male_height)
+p_x_given_female, bin_center_female = generate_pdf(female_height)
+
+# plotting the calculated likelihoods
+plt.bar(bin_center_male, p_x_given_male)
+plt.bar(bin_center_female, p_x_given_female, alpha=0.5)
+plt.show()
+# finding the prior P(X)
+p_male = number_of_male / (number_of_male + number_of_female)
+p_female = number_of_female / (number_of_male + number_of_female)
+
+# plotting determined prior
+plt.figure()
+plt.bar(['m'], [p_male])
+plt.bar(['f'], [p_female])
+plt.show()
+
+# finding the posterior using bayes rule P(Y|X)
+p_male_given_x = p_x_given_male * p_male / (p_x_given_male * p_male + p_x_given_female * p_female)
+p_female_given_x = p_x_given_female * p_female / (p_x_given_male * p_male + p_x_given_female * p_female)
+
+# plotting the calculated posterior
+plt.figure()
+plt.plot(bin_center_male, p_male_given_x, linewidth=4)
+plt.plot(bin_center_female, p_female_given_x, linewidth=4)
+plt.show()
